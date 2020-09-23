45321b4ef9c52ed57455dfd6253345546a3c8fa6
oleg_dm <i@horo.so>
2020-09-23 11:59:17 +0300

    The fourth task has completed

--- ex3.py
+++ ex3.py
@@ -2,7 +2,7 @@
 # Найдите сумму чисел n + nn + nnn.
 # Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
-user_num = int(input('Введите произвольное число '))
+user_num = int(input('Введите целое положительное число'))
 double_num = int(str(user_num) + str(user_num))
 triple_num = int(str(user_num) + str(user_num) + str(user_num))
 print(user_num + double_num + triple_num)
