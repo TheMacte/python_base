27ea82fd3d11724ed68f77c65d8d6d2ae020f9a0
oleg_dm <i@horo.so>
2020-09-23 10:57:17 +0300

    improved the first task

--- ex2.py
+++ ex2.py
@@ -1,3 +1,21 @@
-# 2. Пользователь вводит время в секундах. 
+# 2. Пользователь вводит время в секундах.
 # Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 # Используйте форматирование строк.
+
+user_seconds = int(input('Введите произвольное количество секунд '))
+minuts = user_seconds // 60
+seconds = user_seconds % 60
+hours = minuts // 60
+minuts = minuts % 60
+
+#
+# if seconds < 10:
+#     seconds = '0' + str(seconds)
+# if minuts < 10:
+#     minuts = '0' + str(minuts)
+# if hours < 10:
+#     hours = '0' + str(hours)
+
+# print(hours, ':', minuts, ':', seconds)
+
+print('{:02d}:{:02d}:{:02d}'.format(hours, minuts, seconds))
