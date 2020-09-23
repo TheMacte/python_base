1798a29cecfc3d8518c73e47b50f45924463f5b8
oleg_dm <i@horo.so>
2020-09-23 10:59:25 +0300

    The second task has completed

--- ex2.py
+++ ex2.py
@@ -8,14 +8,4 @@ seconds = user_seconds % 60
 hours = minuts // 60
 minuts = minuts % 60
 
-#
-# if seconds < 10:
-#     seconds = '0' + str(seconds)
-# if minuts < 10:
-#     minuts = '0' + str(minuts)
-# if hours < 10:
-#     hours = '0' + str(hours)
-
-# print(hours, ':', minuts, ':', seconds)
-
 print('{:02d}:{:02d}:{:02d}'.format(hours, minuts, seconds))
