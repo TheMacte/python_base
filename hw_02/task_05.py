2be6d1997eb77261925e9461d2ec30ab0ce65b3a
oleg_dm <i@horo.so>
2020-09-27 21:56:21 +0300

    the task_05 has been completed

--- hw_02/task_05.py
+++ hw_02/task_05.py
@@ -6,3 +6,34 @@
 # Пользователь ввел число 8. Результат: 8, 7, 5, 3, 3, 2.
 # Пользователь ввел число 1. Результат: 7, 5, 3, 3, 2, 1.
 # Набор натуральных чисел можно задать непосредственно в коде, например, my_list = [7, 5, 3, 3, 2].
+
+rating = [7, 5, 3, 3, 2]
+
+try:
+    new_element = int(input('Новый элемент рейтинга '))
+except ValueError:
+    new_element = None
+
+count_exist_element = rating.count(new_element)
+if count_exist_element > 0:
+    rating.reverse()
+    rating.insert(rating.index(new_element), new_element)
+    rating.reverse()
+    # print('Аналогичные элементы есть в списке')
+else:
+    if new_element > rating[0]:
+        rating.insert(0, new_element)
+        # print('Вставить первый элемент')
+    elif new_element < rating[len(rating) - 1]:
+        rating.insert(len(rating), new_element)
+        # print('Вставить последний элемент')
+    else:
+        for element in rating:
+            if element < new_element:
+                rating.insert(rating.index(element), new_element)
+                break
+        # print('Перебор и поиск меньшего')
+
+print(rating)
+
+
