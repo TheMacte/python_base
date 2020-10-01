00dfb41d47ab932ec7956393a35504b1fd78f3da
oleg_dm <i@horo.so>
2020-10-02 00:38:07 +0300

    the task_05 has been finished

--- hw_03/task_5.py
+++ hw_03/task_5.py
@@ -3,3 +3,16 @@
 # введенных чисел будет добавляться к уже подсчитанной сумме. Но если вместо числа вводится специальный символ,
 # выполнение программы завершается. Если специальный символ введен после нескольких чисел, то вначале нужно добавить
 # сумму этих чисел к полученной ранее сумме и после этого завершить программу.
+big_num = 0
+
+while True:
+    user_string = input('Введите числа через пробел или q для завершения: ')
+    if user_string.find('q') != -1:
+        user_string = map(int, user_string[:user_string.index('q')].split())
+        big_num += sum(user_string)
+        print(big_num)
+        break
+    else:
+        user_string = map(int, user_string.split())
+        big_num += sum(user_string)
+        print(big_num)
