b61dec7282e5f825866618a0b9a39b90b5fa22ec
oleg_dm <i@horo.so>
2020-10-01 22:43:22 +0300

    the task_01 has been finished

--- hw_03/task_1.py
+++ hw_03/task_1.py
@@ -1,2 +1,17 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление. Числа запрашивать у
 # пользователя, предусмотреть обработку ситуации деления на ноль.
+
+def degree(*args):
+    if args[1] != 0:
+        return args[0] / args[1]
+    else:
+        return 'На ноль делить нельзя'
+
+try:
+    first_num = float(input('Введите делимое '))
+    second_num = float(input('Введите делитель '))
+except ValueError:
+    first_num = 0
+    second_num = 1
+
+print(degree(first_num, second_num))
