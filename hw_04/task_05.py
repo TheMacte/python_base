e3f3923e86d81317155b51649a27617a1e478b1e
oleg_dm <i@horo.so>
2020-10-05 21:39:26 +0300

    the task_05 has been finished

--- hw_04/task_05.py
+++ hw_04/task_05.py
@@ -2,3 +2,11 @@
 # четные числа от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов
 # списка. Подсказка: использовать функцию reduce().
 
+from functools import reduce
+
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(my_func, range(100, 1001, 2)))
