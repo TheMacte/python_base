d76d4bbcb557dc6f045afdb86f2efcf3c94ea478
oleg_dm <i@horo.so>
2020-10-08 22:31:36 +0300

    the task_05 has been completed

--- hw_05/task5.py
+++ hw_05/task5.py
@@ -1,2 +1,10 @@
 # 5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. Программа
 # должна подсчитывать сумму чисел в файле и выводить ее на экран.
+
+with open('rand_numbers', 'w+') as numbers_file:
+    numbers_file.write('50 32 17 92 22 14 7 13')
+    numbers_file.seek(0)
+    content = numbers_file.read()
+    print(sum(map(int, content.split())))
+
+
